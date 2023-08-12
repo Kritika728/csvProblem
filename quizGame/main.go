1dce3ccb51aeac41549a0eeb0774f961ec6fdaf3
Kritika728 <kritikab728@gamil.com>
2023-08-12 20:49:08 +0530

    added flag and changed while input the answer

--- quizGame/main.go
+++ quizGame/main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"bufio"
 	"encoding/csv"
+	"flag"
 	"fmt"
 	"io"
 	"log"
@@ -20,20 +22,26 @@ type response struct {
 	incorrect int
 }
 
+var filename = flag.String("quizGame.csv", "problem.csv", "csv file that contains quiz questions")
+var timeLimit = flag.Int("15", 30, "Time limit for the quiz")
+
 func main() {
 	var result response
+
 	//file := readFile()
 	//result.total = getTotalQuestion(file)
-	samefile := readFile()
-	result = fetchFileContent(samefile)
 
+	file := readFile()
+	result = fetchFileContent(file)
+
+	//output
 	fmt.Printf("%v/%v ", result.correct, result.total)
 
 }
 
 //readFile : read and parse csv file and return a pointer to a csv.Reader struct
 func readFile() *csv.Reader {
-	folder, err := os.Open("quizGame.csv")
+	folder, err := os.Open(*filename)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -42,24 +50,15 @@ func readFile() *csv.Reader {
 	return file
 }
 
-func getTotalQuestion(file *csv.Reader) int {
-	var count response
-	records, err := file.ReadAll()
-	if err != nil {
-		log.Fatal(err)
-	}
-	count.total = len(records[1:])
-
-	return count.total
-}
-
 //fetchFileContent : read content of file
 func fetchFileContent(file *csv.Reader) response {
 	var result response
 	var userAns int
+	scanner := bufio.NewScanner(os.Stdin)
 	done := make(chan bool)
 
 	//to get the total Number of question which is mentioned in the first row of the file
+	//return err if file is empty
 	data, err := file.Read()
 	if err != nil {
 		log.Fatal(err, " File is empty")
@@ -93,10 +92,13 @@ func fetchFileContent(file *csv.Reader) response {
 			fmt.Println("Question: ", row.question)
 
 			//write the answer of given question
-			fmt.Scanln(&userAns)
 
-			if err != nil {
-				log.Fatal(err)
+			for {
+				userAns, err = readAns(scanner)
+				if err == nil {
+					break
+				}
+				fmt.Println("Please renter ")
 			}
 
 			ans, _ := strconv.Atoi(row.answer)
@@ -111,12 +113,37 @@ func fetchFileContent(file *csv.Reader) response {
 		done <- true
 	}()
 
-	//channel receive the signal
+	//channel receive the data
 	select {
 	case <-done:
 		fmt.Println("You did it")
-	case <-time.After(10 * time.Second):
+	case <-time.After(time.Duration(*timeLimit) * time.Second):
 		fmt.Println("TimeOut")
 	}
 	return result
+}
+
+//getTotalQuestion : get the total number of question
+func getTotalQuestion(file *csv.Reader) int {
+	var count response
+	records, err := file.ReadAll()
+	if err != nil {
+		log.Fatal(err)
+	}
+	count.total = len(records[1:])
+
+	return count.total
+}
+
+func readAns(scanner *bufio.Scanner) (int, error) {
+	var (
+		err error
+		num int
+	)
+	scanner.Scan()
+	input := scanner.Text()
+	if num, err = strconv.Atoi(input); err != nil {
+		fmt.Println("Invalid input. Please enter a valid integer: ", err)
+	}
+	return num, err
 }
